21d65000f46e1c6b6eecd977be1222ac3f065742
Diego Wistuba <diegoswlt@gmail.com>
2021-07-28 17:01:02 -0400

    ad2_parser goes from ad2 to gm notes

--- ad2_parser.py
+++ ad2_parser.py
@@ -1,21 +1,32 @@
 import json
 
-res = {}
+ad2_parsed = {}
 
 with open("wytus.AD2Map", 'r') as f:
     for line in f.readlines():
         if line.startswith(" MAP_KeyMap_"):
             key = str(int(line[12:15]))
             val = int(line[18:-3])
-            res[key] = val
+            ad2_parsed[key] = val
 
 with open("ad2_map.json", 'r') as f:
-    ad2 = json.load(f)
+    ad2_map = json.load(f)
 
-for key in res.keys():
-    if str(res[key]) in list(ad2['note'].keys()):
-        res[key] = ad2['note'][str(res[key])]
+with open("ad2_to_gm.json", 'r') as f:
+    ad2_to_gm = json.load(f)
+
+with open("gm_map.json", 'r') as f:
+    gm_map = json.load(f)
+
+for key in ad2_parsed.keys():
+    ad2_key = str(ad2_parsed[key])
+    if ad2_key in list(ad2_map.keys()):
+        ad2_name = ad2_map[ad2_key]
+        gm_name = ad2_to_gm[ad2_name]
+        try:
+            ad2_parsed[key] = gm_map[gm_name]
+        except KeyError:
+            ad2_parsed[key] = 0
 
 with open("ad2_parsed.json", 'w') as f:
-    json.dump(res, f, indent=2)
-
+    json.dump(ad2_parsed, f, indent=2)
